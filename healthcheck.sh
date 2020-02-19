cbe2ac7e667db00658c670f35e9bf460107adde9
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-19 01:28:20 +0000

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(netcat -z "$(hostname -i)" 3306; echo "${?}")" -ne 0 ]; then
+if [ "$(netcat -z "$(hostname -i)" 3306; echo $?)" -ne 0 ]; then
       echo "MariaDB server not responding on port 3306"
    exit 1
 fi
