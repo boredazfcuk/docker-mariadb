09ede9cbe0e5c802f5dfa4019bb3375f703cb6a8
boredazfcuk <boredazfcuk@hotmail.co.uk>
2021-06-11 09:57:12 +0100

    Please check change.log

--- init-dbs.sh
+++ init-dbs.sh
@@ -17,8 +17,8 @@ InitialiseNextcloud(){
       echo "$(date '+%c') INFO   : Database ${nextcloud_db} already exists"
    fi
    if [ "$(mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" --execute="SELECT User FROM mysql.user;" | grep -c "${nextcloud_db_user}")" = 0 ]; then
-      echo "$(date '+%c') INFO   : Creating user ${nextcloud_db_user} with password: ${nextlcloud_db_password}"
-      mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" --execute="CREATE USER '${nextcloud_db_user}' IDENTIFIED BY '${nextlcloud_db_password}'; GRANT ALL PRIVILEGES ON ${nextcloud_db}.* TO ${nextcloud_db_user}; FLUSH PRIVILEGES;"
+      echo "$(date '+%c') INFO   : Creating user ${nextcloud_db_user} with password: ${nextcloud_db_password}"
+      mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" --execute="CREATE USER '${nextcloud_db_user}' IDENTIFIED BY '${nextcloud_db_password}'; GRANT ALL PRIVILEGES ON ${nextcloud_db}.* TO ${nextcloud_db_user}; FLUSH PRIVILEGES;"
    else
       echo "$(date '+%c') INFO   : Database ${nextcloud_db_user} already exists"
    fi
