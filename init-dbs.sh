64cf7ba220d76fff22261b1bfbc9f198e6e88ee4
boredazfcuk <boredazfcuk@hotmail.co.uk>
2021-06-11 10:57:12 +0100

    Please check change.log

--- init-dbs.sh
+++ init-dbs.sh
@@ -10,12 +10,14 @@ InitialiseKodi(){
 }
 
 InitialiseNextcloud(){
+   echo "$(date '+%c') INFO:    Initialise Nextcloud database"
    if [ "$(mysqlshow --user=root --password="${MYSQL_ROOT_PASSWORD}" | grep -c "${nextcloud_db}")" -ne 0 ]; then
-      echo "$(date '+%c') INFO   : Creating database ${nextcloud_db}"
+      echo "$(date '+%c') INFO   : Creating database: ${nextcloud_db}"
       mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" --execute="CREATE DATABASE ${nextcloud_db};"
    else
       echo "$(date '+%c') INFO   : Database ${nextcloud_db} already exists"
    fi
+   echo "$(date '+%c') INFO:    Initialise Nextcloud user"
    if [ "$(mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" --execute="SELECT User FROM mysql.user;" | grep -c "${nextcloud_db_user}")" = 0 ]; then
       echo "$(date '+%c') INFO   : Creating user ${nextcloud_db_user} with password: ${nextcloud_db_password}"
       mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" --execute="CREATE USER '${nextcloud_db_user}' IDENTIFIED BY '${nextcloud_db_password}'; GRANT ALL PRIVILEGES ON ${nextcloud_db}.* TO ${nextcloud_db_user}; FLUSH PRIVILEGES;"
@@ -33,3 +35,5 @@ if getent hosts kodi >/dev/null 2>&1; then
    InitialiseKodi
 fi
 if getent hosts nextcloud >/dev/null 2>&1; then InitialiseNextcloud; fi
+
+
